533470ee5c8dfb4b24b64ce7ac6f09744b71807c
kokomi <midnightneon2001@gmail.com>
2023-11-12 23:57:34 +0800

    feat: fix responseType

--- src/api/files.js
+++ src/api/files.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 export const getFile=(fileName)=>{
-    return axios.get(`http://127.0.0.1:8080/document/preview?fileName=${fileName}`)
+    return axios.get(`http://127.0.0.1:8080/document/preview?fileName=${fileName}`,{responseType:"arraybuffer"})
 }
