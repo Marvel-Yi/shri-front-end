7f96f1d25fbd7a66c83efaf615f3221e7c0dc579
kokomi <midnightneon2001@gmail.com>
2023-11-14 23:17:20 +0800

    feat: add download and view files

--- src/api/files.js
+++ src/api/files.js
@@ -2,3 +2,7 @@ import axios from 'axios'
 export const getFile=(fileName)=>{
     return axios.get(`http://127.0.0.1:8080/document/preview?fileName=${fileName}`,{responseType:"arraybuffer"})
 }
+
+export const downloadFile=(fileName)=>{
+    return axios.get(`http://127.0.0.1:8080/document/download?fileName=${fileName}`,{responseType:"blob"})
+}
--- src/views/viewMaterials.vue
+++ src/views/viewMaterials.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-    <div style="height: 59px"></div>
-    <el-button @click="viewFile0">click to view file0</el-button>
+    <div style="height: 75px"></div>
+    <el-card style="width: 40%;margin: auto;text-align: left" shadow="never">
+      <div style="margin-left: 20px;margin-top: 20px;padding-bottom: 20px">
+        <span class="title-prefix-_MYP6HvkiQ" ></span><span style="font-weight: bold">EDUCATION CERTIFICATE</span>
+        <div style="margin-top: 30px;margin-left: 30px">
+          <el-button @click="viewFile0">click to view</el-button>
+          <el-button>click to download</el-button>
+        </div>
+      </div>
+      <el-divider></el-divider>
+
+      <div style="margin-left: 20px;margin-top: 40px;padding-bottom: 20px">
+        <span class="title-prefix-_MYP6HvkiQ" ></span><span style="font-weight: bold">LANGUAGE CERTIFICATE</span>
+        <div style="margin-top: 30px;margin-left: 30px">
+          <el-button @click="viewFile1">click to view</el-button>
+          <el-button>click to download</el-button>
+        </div>
+      </div>
+      <el-divider></el-divider>
+
+      <div style="margin-left: 20px;margin-top: 40px;padding-bottom: 20px">
+        <span class="title-prefix-_MYP6HvkiQ" ></span><span style="font-weight: bold">EMPLOYMENT CERTIFICATE</span>
+        <div style="margin-top: 30px;margin-left: 30px">
+          <el-button @click="viewFile2">click to view</el-button>
+          <el-button>click to download</el-button>
+        </div>
+      </div>
+      <div style="height: 50px"></div>
+    </el-card>
+
   </div>
 
 </template>
 
 <script>
-import {getFile} from "../api/files.js";
+import {downloadFile, getFile} from "../api/files.js";
 
 export default {
   name: "viewMaterials",
@@ -27,11 +55,54 @@ export default {
         let url=window.URL.createObjectURL(blob)
         window.open(url,"_blank")
       })
+    },
+    viewFile1(){
+      getFile(this.$data.files.file1).then(res=>{
+        const blob=new Blob([res.data],{type:"application/pdf"})
+        let url=window.URL.createObjectURL(blob)
+        window.open(url,"_blank")
+      })
+    },
+    viewFile2(){
+      getFile(this.$data.files.file2).then(res=>{
+        const blob=new Blob([res.data],{type:"application/pdf"})
+        let url=window.URL.createObjectURL(blob)
+        window.open(url,"_blank")
+      })
+    },
+    downloadFile(type){
+      let fileName=''
+      if(type===0) {
+        fileName=this.$data.files.file0
+      }else if(type===1){
+        fileName=this.$data.files.file1
+      }else{
+        fileName=this.$data.files.file2
+      }
+      downloadFile(fileName).then(res=>{
+        const blob=new Blob([res.data])
+        var fileURL = window.URL.createObjectURL(blob);
+        var fileLink = document.createElement('a')
+        fileLink.href = fileURL
+        fileLink.setAttribute('download',fileName)
+        document.body.appendChild(fileLink)
+        fileLink.click();
+
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-
+.title-prefix-_MYP6HvkiQ {
+  width: 4px;
+  height: 14px;
+  display: inline-block;
+  margin-right: 5px;
+  -webkit-border-radius: 100px;
+  -moz-border-radius: 100px;
+  border-radius: 100px;
+  background: #0068FF;
+}
 </style>
